89f1066c67d82944e9463e972f55f9f137640e1f
Miles O <36418561+stationswift001@users.noreply.github.com>
2022-10-22 18:31:53 -0500

    Initial Commit
    send help i have no clue what the code i am writing does

---
+++ list.cs
@@ -0,0 +1,30 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+		ConsoleKeyInfo cki;
+        List<string> list = new List<string>();
+        //into and instructions
+        Console.WriteLine("Welcome to the list!");
+        Console.WriteLine("Type something and press enter to add it!");
+		cki = Console.ReadKey();
+        if((cki.Modifiers & ConsoleModifiers.Alt) = 0)
+		string latestTest = Console.ReadLine();
+        //Console.WriteLine(latestTest);
+        list.Add(latestTest);
+        //int numberList = 0;
+		//numberList = list.FindIndex() + 1;
+		//Console.WriteLine(numberList);
+        //list.ForEach( + Console.WriteLine);
+		foreach(String item in list){
+			Console.WriteLine((list.IndexOf(item) + 1) + ": " + item);
+		}
+    }
+}
